894d5a8eb305ffc244135d12b54a2eacb23bd39b
Thomas Pagesy <thomas@MacBook-Pro-de-Thomas.local>
2026-03-22 16:08:16 +0100

    fixed gen pbs

--- osc_server.py
+++ osc_server.py
@@ -12,65 +12,37 @@ from stable_audio_tools.models.pretrained import get_pretrained_model
 from stable_audio_tools.inference.generation import generate_diffusion_cond
 from huggingface_hub import login
 
-torch.cuda.empty_cache()
-torch.cuda.reset_peak_memory_stats()
-torch.cuda.reset_accumulated_memory_stats()
-
 path = os.getcwd()
 
 device = "mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu"
 
 client = SimpleUDPClient("127.0.0.1", 9001)
 
-print(path)
-print(os.listdir(path))
 
-if "model.pt" not in os.listdir(path + "\\models"):
-    # log = input("Enter huggingface loggin")
+if "model.pt" not in os.listdir(path + "/models"):
     if "log.txt" in os.listdir(path) :
-        with open(path +  "\\log.txt" ) as txt_file:
+        with open(path +  "/log.txt" ) as txt_file:
             log = txt_file.readlines()
     else:
         log = input("Enter huggingface loggin")
     login(str(log))
     
     model, model_config = get_pretrained_model("stabilityai/stable-audio-open-small")#.to(device)
+  
     model = model.to(device)
-    torch.save(model, path + "\\models\\model.pt")
+    torch.save(model, path + "/models/model.pt")
     
 else: 
-    model = torch.load(path + "\\models" + "\\model.pt", weights_only=False).to(device)
-    with open(path +  "\\base_model_config.json") as json_file:
+    model = torch.load(path + "/models" + "/model.pt", weights_only=False).to(device)
+
+    with open(path +  "/base_model_config.json") as json_file:
         model_config = json.load(json_file)
-# model, model_config = get_pretrained_model(path + "\\models" + "\\model.pt")#.to(device)
-# model = model.to(device)
+
 sample_rate = model_config["sample_rate"]
+sample_size = model_config["sample_size"]
 print("model sample rate", sample_rate)
 
-# def generate_diffusion_cond(model, steps, cfg_scale, conditioning, sample_size, init_audio, init_noise_level, sampler_type, seed, device):
-#     torch.manual_seed(seed)
-#     model.eval()
-    
-#     init_audio_tensor = torch.tensor(init_audio[1]).to(device).unsqueeze(0)
-#     noise = torch.randn(init_audio_tensor.shape).to(device) * init_noise_level
-#     audio = init_audio_tensor + noise
 
-#     for step in range(steps):
-#         with torch.no_grad():
-#             audio = model(audio, conditioning, cfg_scale)
-    
-#     return audio
-
-# def sample_flow_pingpong(model, audio, steps, cfg_scale, seed):
-#     torch.manual_seed(seed)
-#     model.eval()
-    
-#     audio = audio.unsqueeze(0)
-#     for step in range(steps):
-#         with torch.no_grad():
-#             audio = model(audio, cfg_scale)
-    
-#     return audio.squeeze(0)
 
 def stable_gen(addr, arg1, arg2, arg3, arg4, arg5, arg6, arg7):
     global model, path, sample_rate
@@ -91,47 +63,29 @@ def stable_gen(addr, arg1, arg2, arg3, arg4, arg5, arg6, arg7):
     print(f"Arg5: {prompt}")
     print(f"arg6 : {cumulate}")
     print(f"arg7 : {n_slices}")
-    print(path + '\\take.wav')
+    print(path + '/take.wav')
     
-    in_audio, sr = librosa.load(path + "\\take.wav", mono = False, sr=sample_rate)
-    # in_audio, sr = sf.read(path + "\\take.wav", samplerate=sample_rate, fill_value=0)
+    in_audio, sr = librosa.load(path + "/take.wav", mono = False, sr = None)
     
     in_audio = in_audio.T
    
     print("audio shape", in_audio.shape)
-    while len(in_audio) == 0:
-        in_audio, sr = sf.read(path + "\\take.wav")
-        
-    
-    in_audio = np.nan_to_num(in_audio).astype(np.float32)
-    print("in audio shape", in_audio.shape)   
-    if len(in_audio.shape) < 2:
-        in_audio = np.stack([in_audio, in_audio], axis = 0) 
-        print("new audio shape", in_audio.shape)
-    
     amp_init = np.max(np.abs(in_audio)) 
-    in_audio = in_audio/amp_init * 0.999
+    in_audio = np.nan_to_num(in_audio)
 
     if int(sr) != int(sample_rate):
-        in_audio = librosa.resample(in_audio.T, orig_sr=sr, target_sr=sample_rate, mono = False).T  
+        in_audio = librosa.resample(in_audio.T, orig_sr=sr, target_sr=sample_rate).T  
     
-    if cumulate > 0:
-        gen_audio, sr = sf.read(path + "\\gen.wav")
-        while len(gen_audio) == 0:
-            gen_audio, sr = sf.read(path + "\\gen.wav")
-        gen_audio = np.nan_to_num(gen_audio).astype(np.float32)
-        print("gen audio shape", gen_audio.shape)
-        # if len(gen_audio.shape) < 2:
-        #     gen_audio = np.stack([gen_audio, gen_audio], axis = 0) 
-        # #     print("new audio shape", gen_audio.shape)
-        # if sr != sample_rate:
-        #     gen_audio = librosa.resample(gen_audio, orig_sr=sr, target_sr=sample_rate
-        max_gen = np.max(np.abs(gen_audio)) 
-        if max_gen  > 0 :
-            gen_audio = gen_audio/max_gen * 0.999
+    if cumulate > 0.1:
+        gen_audio, sr_gen = librosa.load(path + "/gen.wav", mono = False, sr = None)
+        gen_audio = gen_audio.T
         
-        if sr != sample_rate:
-            gen_audio = librosa.resample(gen_audio.T, orig_sr=sr, target_sr=sample_rate, mono = False).T
+            
+        gen_audio = np.nan_to_num(gen_audio)
+        print("gen audio shape", gen_audio.shape)
+        
+        if sr_gen != sample_rate:
+            gen_audio = librosa.resample(gen_audio.T, orig_sr=sr_gen, target_sr=sample_rate).T
              
         if gen_audio.shape[0] < in_audio.shape[0]:
             gen_audio = np.pad(gen_audio, ((0, in_audio.shape[0]- gen_audio.shape[0]), (0,0)), constant_values=0)
@@ -141,10 +95,7 @@ def stable_gen(addr, arg1, arg2, arg3, arg4, arg5, arg6, arg7):
         in_audio = in_audio*(1-cumulate) + gen_audio*cumulate     
  
     blocksize = in_audio.shape[0]
-    in_audio = np.nan_to_num(in_audio).astype(np.float16)
     max_index = np.argmax(in_audio)
-    # in_audio = np.mean(in_audio, axis = 0)[np.newaxis, :]
-    print("blocksize", blocksize)
     
     conditioning = [{
                     "prompt": prompt,
@@ -153,57 +104,45 @@ def stable_gen(addr, arg1, arg2, arg3, arg4, arg5, arg6, arg7):
                     }]
     
     neg_conditioning = [{
-                    "prompt": "Bad quality, silence",
+                    "prompt": "Bad quality, silence, noisy",
                     "seconds_total": 12
                     # "seconds_total": np.ceil(blocksize/sample_rate).astype(int)
                     }]
-    
-    final_audio = []    
-    # sliced_audio = np.transpose(np.reshape(in_audio, [in_audio.shape[-2], n_slices, blocksize//n_slices]  ), (1,0,2))
-    
-    #for i in range(n_slices):  
+     
+   
     if seed <= 0:
-        seed_gen = np.random.randint(1, 99999)  
+        seed_gen = -1
     else:
         seed_gen = seed    
-    # slice_audio = sliced_audio[:, i, :]  
-    # print(np.sum(slice_audio))
-    # slice_audio = np.mean(slice_audio, axis=0)[np.newaxis]
+
     print("audio shape : ", in_audio.shape)
+    in_audio = np.mean(in_audio, axis = -1)[:, np.newaxis]
+    in_audio = in_audio/np.max(np.abs(in_audio))
     audio_seed = torch.from_numpy(in_audio.T).to(torch.float32)
-    # audio_seed = audio_seed.to(device)
+
     output = generate_diffusion_cond(
                                     model,
                                     steps=n_step,
                                     cfg_scale=scale,
                                     conditioning=conditioning,
-                                    negative_conditioning=neg_conditioning,
-                                    sample_size=int(blocksize//n_slices),
-                                    init_audio = (int(sample_rate), audio_seed),
-                                    init_noise_level = init_noise,
-                                    sampler_type="pingpong",
+                                    # negative_conditioning=neg_conditioning,
+                                    sample_size=blocksize,
+                                    init_audio = ([sample_rate, audio_seed]),
+                                    init_noise_level = float(init_noise),
+                                    sampler_type= "pingpong",
                                     seed = seed_gen,
                                     device=device
                                     )
     
-    output = rearrange(output, "b d n -> d (b n)")
     
-    output = output.div(torch.max(torch.abs(output))).clamp(-1, 1).cpu()
-    # output = output.cpu()
-    # print(output)
-    # output = output/np.max(np.abs(output))
-    output = amp_init*output * 0.999
+    output = output[0].T.float().div(torch.max(torch.abs(output))).clamp(-1, 1).cpu().numpy()
+
+    output  = librosa.resample(output[:blocksize].T, orig_sr=sample_rate, target_sr=sr).T
+
+    print(output.shape)
     
-    final_audio.append(output.numpy().T)
-    torch.cuda.empty_cache()
+    sf.write(path + '/gen.wav', output, sr, format = "wav")
     
-    final_audio = np.concatenate(final_audio, axis = 0)
-    final_audio = np.pad(final_audio, [[0, blocksize - len(final_audio)],[0, 0]])
-    final_audio = np.nan_to_num(final_audio)
-    print(final_audio)
-    print("out shape", final_audio.shape)
-    # final_audio = librosa.resample(final_audio, orig_sr=sample_rate, target_sr=sr)
-    sf.write(path + '\\gen.wav', final_audio, sample_rate, format = "wav")#, subtype='PCM_24')
     client.send_message("127.0.0.1:9001", ["bang"])
 
 disp = dispatcher.Dispatcher()
